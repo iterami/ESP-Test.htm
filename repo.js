609f2fab7b5b736d1648ecab4bbcb6d079a32aa6
Jan Hořava <czechinthemail@gmail.com>
2026-03-10 12:56:57 -0700

    formatted integer result and added newline

--- repo.js
+++ repo.js
@@ -2,13 +2,15 @@
 
 function check(){
     const integer = core_random_integer(1e6) + 1;
-    let result = integer + ' was generated. You ';
+    let result = core_number_format({
+        'number': integer,
+      }) + ' was generated.<br>You ';
 
     result += Number.parseInt(core_elements.answer.value, 10) === integer
       ? 'might have ESP!'
       : 'probably don\'t have ESP.';
 
-    core_elements.result.textContent = result;
+    core_elements.result.innerHTML = result;
 }
 
 function repo_init(){
